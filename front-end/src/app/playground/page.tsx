dc83d4548256019c1d0bd7e6df8af72fbf991c71
Minh Au <auduongminh2004@gmail.com>
2024-10-05 16:04:08 -0700

    sample playground

---
+++ front-end/src/app/playground/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+import React, { useCallback } from 'react';
+import {
+   ReactFlow,
+   useNodesState,
+   useEdgesState,
+   addEdge,
+   Controls,
+   Background,
+   BackgroundVariant,
+} from '@xyflow/react';
+
+import '@xyflow/react/dist/style.css';
+
+const initialNodes = [
+   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+export default function App() {
+   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+   const onConnect = useCallback(
+      (params: any) => setEdges((eds) => addEdge(params, eds)),
+      [setEdges],
+   );
+
+   return (
+      <div style={{ width: '100vw', height: '100vh' }}>
+         <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+         >
+            <Controls />
+            <Background color="#ccc" variant={BackgroundVariant.Cross} />
+         </ReactFlow>
+
+      </div>
+   );
+}
