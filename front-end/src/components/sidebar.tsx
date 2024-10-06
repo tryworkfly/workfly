abbc0665472214bad60fd126411c4dc9ac3a16c7
Minh Au <auduongminh2004@gmail.com>
2024-10-06 09:53:46 -0700

    add scrollable bar

--- front-end/src/components/sidebar.tsx
+++ front-end/src/components/sidebar.tsx
@@ -19,7 +19,8 @@ export default function Sidebar(props: {
 
   return (
     <div
-      className="absolute shadow-md shadow-slate-200 inset-y-0 my-auto flex flex-col items-center w-1/4 h-5/6 py-[2rem] px-[0.9375rem] ml-4 gap-[1.4375rem] shrink-0 rounded-sm bg-white z-10"
+      className="absolute shadow-md shadow-slate-200 inset-y-0 my-auto flex flex-col items-center w-1/4 h-5/6 py-[2rem] px-[0.9375rem] ml-4 gap-[1.4375rem] shrink-0 rounded-sm bg-white z-10
+        overflow-scroll"
       onDragOver={(e) => {
         e.preventDefault();
       }}
