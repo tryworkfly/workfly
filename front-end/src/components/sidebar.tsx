5085af5e742949aea17c39aa1665092fbacfc3a5
aaneres <159407812+aaneres@users.noreply.github.com>
2024-10-05 19:43:23 -0700

    Created empty sidebar

---
+++ front-end/src/components/sidebar.tsx
@@ -0,0 +1,5 @@
+export default function Sidebar() {
+    return (
+        <div className = "absolute shadow-md shadow-slate-200 inset-y-0 my-auto flex flex-col items-center w-1/4 h-5/6 py-[2rem] px-[0.9375rem] ml-4 gap-[1.4375rem] shrink-0 rounded-sm bg-white">
+        </div>
+    )}
