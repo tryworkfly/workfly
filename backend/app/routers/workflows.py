8304c7994d6a36e7ee4fc20d3b94b5b866590180
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-10-27 23:38:35 -0700

    Make db clients context managers to properly close sessions after use

--- backend/app/routers/workflows.py
+++ backend/app/routers/workflows.py
@@ -10,26 +10,26 @@ router = APIRouter(prefix="/workflows")
 
 @router.get("/", response_model=list[WorkflowPublic])
 async def list_workflows():
-    workflow_client = WorkflowClient()
-    return list(workflow_client.get_all())
+    with WorkflowClient() as workflow_client:
+        return list(workflow_client.get_all())
 
 
 @router.get("/{workflow_id}", response_model=WorkflowPublic)
 async def get_workflow(workflow_id: uuid.UUID):
-    workflow_client = WorkflowClient()
-    workflow = workflow_client.get(workflow_id)
-    if workflow is None:
-        raise HTTPException(status_code=404, detail="Workflow not found")
+    with WorkflowClient() as workflow_client:
+        workflow = workflow_client.get(workflow_id)
+        if workflow is None:
+            raise HTTPException(status_code=404, detail="Workflow not found")
     return workflow
 
 
 @router.post("/", response_model=WorkflowPublic)
 async def create_workflow(workflow: WorkflowCreate):
-    workflow_client = WorkflowClient()
-    return workflow_client.create(workflow)
+    with WorkflowClient() as workflow_client:
+        return workflow_client.create(workflow)
 
 
 @router.put("/{workflow_id}", response_model=WorkflowPublic)
 async def update_workflow(workflow_id: uuid.UUID, workflow: WorkflowCreate):
-    workflow_client = WorkflowClient()
-    return workflow_client.put(workflow_id, workflow)
+    with WorkflowClient() as workflow_client:
+        return workflow_client.put(workflow_id, workflow)
